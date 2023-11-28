df17f9d9a2dfd703dad0d6b5ffa0f12df657a53f
AlexisEstela <aestelalozano@gmail.com>
2023-11-28 17:01:45 -0500

    avances hasta sign up y login

---
+++ controllers/authController.js
@@ -0,0 +1,68 @@
+// importamos el modelo del usuario
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+
+// handle errors
+
+const handle_Errors = (err) =>{
+    console.log(err.message, err.code)
+    let errors = { email : '', password: ''}
+
+    // duplicate error code
+    if (err.code === 11000){
+        errors.email = 'that email is already registered'
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) =>{
+            errors[properties.path] = properties.message
+        })
+    }
+
+    return errors
+}
+const maxAge = 3*24*60*60
+const createToken = (id) => {
+    return jwt.sign({id}, 'net ninja secret', {
+        expiresIn: maxAge
+    })
+
+}
+
+module.exports.signup_get = (req,res) => {
+    res.render('signup')
+}
+
+module.exports.login_get = (req,res) => {
+    res.render('login')
+}
+
+// vista para crear un nuevo usuario usando el modelo creado en User.js
+module.exports.signup_post = async (req,res) => {
+    const { email, password } = req.body
+
+    try{
+        const user = await User.create({ email, password})
+        const token = createToken(user._id)
+        res.cookie('jwt',token, {httpOnly:true,maxAge: maxAge*1000})
+        res.status(201).json({ user: user._id})
+
+
+    }catch (err){
+        const errors =  handle_Errors(err)
+        res.status(400).json({errors})
+    }
+}
+
+module.exports.login_post = async (req,res) => {
+    const { email, password } = req.body
+
+    try{
+        const user= await User.login(email,password)
+        res.status(200).json({user: user._id})
+    }catch{
+        res.status(400).json({})
+    }
+
+}
